f64fa8aef75e3b4fb82ff2bbac8097a84ec348cd
abeermosaad <abeermosaad69@gmail.com>
2023-10-29 16:44:28 +0200

    Make import not at top

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""
-Blueprint for the api
-"""
-
-from api.v1.views.states import *
-from api.v1.views.index import *
+"""Blueprint for the api"""
 from flask import Blueprint, jsonify
 
 app_views = Blueprint('app_views', __name__,
                       url_prefix=('/api/v1'))
+
+from api.v1.views.index import *
+from api.v1.views.states import *
